3a89cb1927d7e7d72f759ad360b1e71519173d3d
XIN <78128788+RiStar-GZX@users.noreply.github.com>
2023-01-28 18:34:39 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,18 +1,9 @@
 /**
  * Cidyright (C), 2022-2023, github.com/RiStar-GZX.
- * File name: main.c     // 文件名
- * Author:Ristar-GZX  Version:V0.0.1    Date:2023.1.5 
- * Description:    // 用于详细说明此程序文件完成的主要功能，与其他模块
-                   // 或函数的接口，输出值、取值范围、含义及参数间的控
-                   // 制、顺序、独立或依赖等关系
- * Others:         // 其它内容的说明
- * Function List:  // 主要函数列表，每条记录应包括函数名及功能简要说明
-    1. main
- * History:        // 修改历史记录列表，每条修改记录应包括修改日期、修改
-                  // 者及修改内容简述
-    1. Date:
-       Author:
-       Modification:
+ * File name: main.c     
+ * Author:Ristar-GZX  Version:V0.0.2    Date:2023.1.28 
+ * Description:    
+      这个示例程序是在linux下通过xdotool来控制鼠标移动，这里是被控端
  */
 #include <stdio.h>
 #include <sys/stat.h>
@@ -37,10 +28,6 @@ int my_slot(sig_id_t sig_id,XLsig_pak * pak)
         sprintf(s,"xdotool mousemove %d %d\n",*data1,*data2);
         system(s);
         system("clear");
-        printf("\n\n\n%d\n\n\n",*data3);
-        /*if(data3>=10)sprintf(s,"xdotool mouseup %d %d\n",*data3-10);
-        else sprintf(s,"xdotool mousedown %d %d\n",*data3);
-        system(s);*/
         return 1;
 }
 
